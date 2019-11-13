bfbb919d14e4e621edba71341162d5823b667f4c
William Stephens <william.a.stephens17@gmail.com>
2019-11-13 09:12:12 -0600

    Morning Javascript warmup.

--- js/calculateRectangleArea.js
+++ js/calculateRectangleArea.js
@@ -1,7 +1,8 @@
 function calculateRectangleArea(length, width) {
-    return length * width;
+    return (length * width);
 }
 
+//.log + tab is the shortcut to console.log() something.
 console.log(calculateRectangleArea(10, 23));
 //230
 console.log(calculateRectangleArea("tree", 48));
