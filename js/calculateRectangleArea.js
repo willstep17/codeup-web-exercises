479e6af3d35cb62cb70a351c286eb2a79821ebe3
William Stephens <william.a.stephens17@gmail.com>
2019-11-13 09:05:01 -0600

    Morning Update

---
+++ js/calculateRectangleArea.js
@@ -0,0 +1,8 @@
+function calculateRectangleArea(length, width) {
+    return length * width;
+}
+
+console.log(calculateRectangleArea(10, 23));
+//230
+console.log(calculateRectangleArea("tree", 48));
+//NaN
