36e68c204baba62930fb0154c7c07de74ad7b948
William Stephens <william.a.stephens17@gmail.com>
2019-11-18 10:15:23 -0600

    Media Queries

--- js/lastItem.js
+++ js/lastItem.js
@@ -1,11 +1,23 @@
-function lastItem(arr) {
-    return arr[arr.length - 1];
+function lastItem(inputArray) {
+    return inputArray[inputArray.length - 1];
+}
+
+//Alternative way to declare a function.
+var lastItemTwo = function (inputArray) {
+    return inputArray[inputArray.length - 1];
+}
+
+function lastItemSlice(inputArray) {
+    return stuff.slice(-1)[0];
 }
 
 var animals = ["Cow", "Chicken", "Bear"];
 var numbers = [43, 52, 75, 1, 74, 90];
 var stuff = ["Toolbox", 42, 2020, "Yoyo", "Yoko Ono"];
+var classMembers = ["Will", "Matt", "Noland", "Faith"];
 
 console.log(lastItem(animals));
 console.log(lastItem(numbers));
-console.log(lastItem(stuff));
+console.log(lastItemTwo(stuff));
+console.log(lastItemTwo(classMembers));
+console.log(lastItemSlice(animals));
