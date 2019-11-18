c17dddb8a66a443f3dc95b4dfac0e816abc8d6d7
William Stephens <william.a.stephens17@gmail.com>
2019-11-18 09:21:54 -0600

    Morning JavaScript

---
+++ js/lastItem.js
@@ -0,0 +1,11 @@
+function lastItem(arr) {
+    return arr[arr.length - 1];
+}
+
+var animals = ["Cow", "Chicken", "Bear"];
+var numbers = [43, 52, 75, 1, 74, 90];
+var stuff = ["Toolbox", 42, 2020, "Yoyo", "Yoko Ono"];
+
+console.log(lastItem(animals));
+console.log(lastItem(numbers));
+console.log(lastItem(stuff));
