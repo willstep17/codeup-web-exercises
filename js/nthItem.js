fab8372ff624fe9a42153a8e4ae2c6833e7f29a1
William Stephens <william.a.stephens17@gmail.com>
2019-11-19 09:22:12 -0600

    Update

--- js/nthItem.js
+++ js/nthItem.js
@@ -1,15 +1,16 @@
-function nthItem (inputArray, item) {
-    return inputArray[item];
+function nthItem (inputArray, n) {
+    return inputArray[n - 1];
 }
 
-var nthItemTwo = function (inputArray, item) {
-    return inputArray[item];
+var nthItemTwo = function (inputArray, n) {
+    return inputArray[n - 1];
 }
 
 var animals = ["Cat", "Dog", "Seal", "Panda"];
-var numbers = [0, 1, 2, 3, 4, 5, 6,];
+var numbers = [0, 1, 2, 3, 4, 5, 6];
 var mixed = ["CodeUp", 43, 74, "November", "Keanu", 12];
 
 console.log(nthItem(animals, 2));
 console.log(nthItem(animals, 0));
 console.log(nthItemTwo(mixed, 4));
+console.log(nthItem(numbers, 2));
