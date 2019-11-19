22b25a099be05388e34fec9294b4bba6db510968
William Stephens <william.a.stephens17@gmail.com>
2019-11-19 09:16:34 -0600

    Morning Warmups

---
+++ js/nthItem.js
@@ -0,0 +1,15 @@
+function nthItem (inputArray, item) {
+    return inputArray[item];
+}
+
+var nthItemTwo = function (inputArray, item) {
+    return inputArray[item];
+}
+
+var animals = ["Cat", "Dog", "Seal", "Panda"];
+var numbers = [0, 1, 2, 3, 4, 5, 6,];
+var mixed = ["CodeUp", 43, 74, "November", "Keanu", 12];
+
+console.log(nthItem(animals, 2));
+console.log(nthItem(animals, 0));
+console.log(nthItemTwo(mixed, 4));
