65fc67469b4ef49110e3e325f31c523242449d3b
William Stephens <william.a.stephens17@gmail.com>
2019-11-25 09:18:18 -0600

    JavaScript warmups

---
+++ js/greaterThanTen.js
@@ -0,0 +1,10 @@
+function greaterThanTen(number) {
+    return (number > 10);
+}
+
+console.log(greaterThanTen(2));
+console.log(greaterThanTen(12));
+console.log(greaterThanTen(10));
+console.log(greaterThanTen(-10));
+console.log(greaterThanTen(0));
+console.log(greaterThanTen(40));
