0cafa2c47f4ba542820bcd75fc42da38f7ced113
William Stephens <william.a.stephens17@gmail.com>
2019-11-25 16:33:25 -0600

    Update

--- js/validCredentials.js
+++ js/validCredentials.js
@@ -1,30 +1,30 @@
 //Data Types Exercise 4
 function validateCredentials(inputUsername, inputPassword) {
-    var credentials = [false, false, false, false];
+    var credentialKey = [false, false, false, false];
     if(inputPassword.length > 5) {
-        credentials[0] = true;
+        credentialKey[0] = true;
     } else {
         console.log("Your username needs to be at least 5 characters long.");
     }
     if(inputPassword.indexOf(inputUsername) === -1) {
-        credentials[1] = true;
+        credentialKey[1] = true;
     } else {
         console.log("Your password cannot contain your username.");
     }
     if(inputUsername.length < 21) {
-        credentials[2] = true;
+        credentialKey[2] = true;
     } else {
         console.log("Your username cannot be longer than 20 characters.");
     }
-    if(inputUsername.length === inputUsername.trim().length) {
-        credentials[3] = true;
+    if(inputUsername === inputUsername.trim()) {
+        credentialKey[3] = true;
     } else {
         console.log("Your username cannot contain whitespace before or after it.");
     }
-    return credentials;
+    return credentialKey;
 }
 
-function acceptCredentials(inputCredentials) {
+function credentialLock(inputCredentials) {
     for(var i = 0; i < inputCredentials.length; i++) {
         if(inputCredentials[i] === false) {
             return "INVALID CREDENTIALS";
@@ -36,4 +36,4 @@ function acceptCredentials(inputCredentials) {
 var username = 'codeup';
 var password = 'notastrongpassword';
 
-console.log(acceptCredentials(validateCredentials(username, password)));
+console.log(credentialLock(validateCredentials(username, password)));
