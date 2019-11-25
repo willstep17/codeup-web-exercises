c537bfaf5f665b5f99f38bfb1e349df8c9b7dd94
William Stephens <william.a.stephens17@gmail.com>
2019-11-24 21:23:08 -0600

    Finishing up design project.

--- js/put2.js
+++ js/put2.js
@@ -4,7 +4,16 @@ function put2(inputArray) {
 }
 
 var testArray = [23, 41, 4, 64, 33];
-var testArray2 = ["Dog", "Vivian", "Daniel", 43, 2];
+// var testArray2 = ["Dog", "Vivian", "Daniel", 43, 2];
 
-console.log(put2(testArray));
-console.log(put2(testArray2));
+var newArray = put2(testArray);
+
+console.log(newArray);
+
+for (var i=0; i<newArray.length; i++) {
+    console.log(newArray[i]);
+}
+
+// console.log(put2(testArray));
+// console.log(testArray);
+// console.log(put2(testArray2));
