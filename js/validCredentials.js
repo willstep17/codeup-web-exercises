a0d32ae87c48d94c58e4399518a301d2850bbf76
William Stephens <william.a.stephens17@gmail.com>
2019-12-02 11:32:10 -0600

    More alerts in validCredentials

--- js/validCredentials.js
+++ js/validCredentials.js
@@ -4,22 +4,22 @@ function buildCredentialKey(inputUsername, inputPassword) {
     if(inputPassword.length > 5) {
         credentialKey[0] = true;
     } else {
-        console.log("Your username needs to be at least 5 characters long.");
+        alert("Your username needs to be at least 5 characters long.");
     }
     if(inputPassword.indexOf(inputUsername) === -1) {
         credentialKey[1] = true;
     } else {
-        console.log("Your password cannot contain your username.");
+        alert("Your password cannot contain your username.");
     }
     if(inputUsername.length < 21) {
         credentialKey[2] = true;
     } else {
-        console.log("Your username cannot be longer than 20 characters.");
+        alert("Your username cannot be longer than 20 characters.");
     }
     if(inputUsername === inputUsername.trim()) {
         credentialKey[3] = true;
     } else {
-        console.log("Your username cannot contain whitespace before or after it.");
+        alert("Your username cannot contain whitespace before or after it.");
     }
     return credentialKey;
 }
@@ -36,4 +36,4 @@ function credentialLock(credentialKey) {
 var username = prompt("Enter a username: ");
 var password = prompt("Enter a password: ");
 
-console.log(credentialLock(buildCredentialKey(username, password))); //VALID CREDENTIALS
+alert(credentialLock(buildCredentialKey(username, password))); //VALID CREDENTIALS
