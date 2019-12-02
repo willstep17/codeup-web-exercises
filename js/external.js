d8c9d81d1cfe6515cb501b0ed48f4bd670c3ad34
William Stephens <william.a.stephens17@gmail.com>
2019-12-02 16:14:29 -0600

    updating external.js

--- js/external.js
+++ js/external.js
@@ -1,6 +1,8 @@
-"use strict"
+//IIFE - Immediately Invoked Function Expression (Protects the global scope)
+(function(){
+"use strict";
 
-//***** JavaScript with HTML exercises *****
+// ***** JavaScript with HTML exercises *****
 // console.log("Hello from external JavaScript.");
 //
 // alert("Welcome to my website!");
@@ -9,28 +11,28 @@
 //
 // alert("Great, " + favoriteColor + " is my favorite color too!");
 
-//***** Data Types Exercise 3-A *****
-// function totalPrice(movies, pricePerDay) {
-//     return "Renting " + movies[0].name + " for " + movies[0].days + " day(s), " +
-//             movies[1].name + " for " + movies[1].days + " day(s), " +
-//             " and " + movies[2].name + " for " + movies[2].days + " day(s) " +
-//             " at a daily rate of $" + pricePerDay + " costs $" +
-//             ((movies[0].days + movies[1].days + movies[2].days) * pricePerDay);
-// }
-//
-// var mermaid = {days: 0, name: "The Little Mermaid"};
-// var bear = {days: 0, name: "Brother Bear"};
-// var hercules = {days: 0, name: "Hercules"};
-// var inputMovies = [mermaid, bear, hercules];
-//
-// var inputPricePerDay = Number(prompt("How many dollars does it cost to rent a movie for a day?"));
-// mermaid.days = Number(prompt("How many days did you rent The Little Mermaid for?"));
-// bear.days = Number(prompt("How many days did you rent Brother for?"));
-// hercules.days = Number(prompt("How many days did you rent Hercules for?"));
-//
-// alert(totalPrice(inputMovies, inputPricePerDay));
+// ***** Data Types Exercise 3-A *****
+function totalPrice(movies, pricePerDay) {
+    return "Renting " + movies[0].name + " for " + movies[0].days + " day(s), " +
+            movies[1].name + " for " + movies[1].days + " day(s), " +
+            " and " + movies[2].name + " for " + movies[2].days + " day(s) " +
+            " at a daily rate of $" + pricePerDay + " costs $" +
+            ((movies[0].days + movies[1].days + movies[2].days) * pricePerDay);
+}
 
-//***** Data Types Exercise 3-B *****
+var mermaid = {days: 0, name: "The Little Mermaid"};
+var bear = {days: 0, name: "Brother Bear"};
+var hercules = {days: 0, name: "Hercules"};
+var inputMovies = [mermaid, bear, hercules];
+
+var inputPricePerDay = Number(prompt("How many dollars does it cost to rent a movie for a day?"));
+mermaid.days = Number(prompt("How many days did you rent The Little Mermaid for?"));
+bear.days = Number(prompt("How many days did you rent Brother for?"));
+hercules.days = Number(prompt("How many days did you rent Hercules for?"));
+
+alert(totalPrice(inputMovies, inputPricePerDay));
+
+// ***** Data Types Exercise 3-B *****
 // function pay(salary, hoursWorked) {
 //     return salary * hoursWorked;
 // }
@@ -52,7 +54,7 @@
 // alert("Your total salary for the week is: $" +
 // (pay(googleRate, googleHours) + pay(amazonRate, amazonHours) + pay(facebookRate, facebookHours)));
 
-//***** Data Types Exercise 3-C *****
+// ***** Data Types Exercise 3-C *****
 // function canEnroll(full, conflict) {
 //     return (!full && !conflict) ? "You can enroll." : "You cannot enroll.";
 // }
@@ -65,18 +67,20 @@
 //
 // alert(canEnroll(isFull, scheduleConflict));
 
-//***** Data Types Exercise 3-D *****
-function offerValid(valid, numberOfProducts, premium) {
-    return (valid && (numberOfProducts > 2 || premium)) ? "Offer Valid" : "Offer Invalid";
-}
+// ***** Data Types Exercise 3-D *****
+// function offerValid(valid, numberOfProducts, premium) {
+//     return (valid && (numberOfProducts > 2 || premium)) ? "Offer Valid" : "Offer Invalid";
+// }
+//
+// var inputValid = confirm("Is the offer still valid?");
+// console.log(typeof(inputValid));
+//
+// var inputNumberOfProducts = parseInt(prompt("How many products are you ordering?"), 10);
+// console.log(typeof(inputNumberOfProducts));
+//
+// var inputPremium = confirm("Do you have a premium membership?");
+// console.log(typeof(inputPremium));
+//
+// alert(offerValid(inputValid, inputNumberOfProducts, inputPremium));
 
-var inputValid = confirm("Is the offer still valid?");
-console.log(typeof(inputValid));
-
-var inputNumberOfProducts = parseInt(prompt("How many products are you ordering?"), 10);
-console.log(typeof(inputNumberOfProducts));
-
-var inputPremium = confirm("Do you have a premium membership?");
-console.log(typeof(inputPremium));
-
-alert(offerValid(inputValid, inputNumberOfProducts, inputPremium));
+})();
