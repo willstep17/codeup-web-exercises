7d0fc10dd5e401668b66e4eadbfb8f30a0364672
William Stephens <william.a.stephens17@gmail.com>
2019-12-02 12:28:31 -0600

    More user input for Salary

--- js/external.js
+++ js/external.js
@@ -29,38 +29,43 @@ hercules.days = parseInt(prompt("How many days did you rent Hercules for?"), 10)
 alert(totalPrice(mermaid, bear, hercules, inputPricePerDay));
 
 //Data Types Exercise 3-B
-// function pay(salary, hoursWorked) {
-//     return salary * hoursWorked;
-// }
-//
-// var googleSal = 400, amazonSal = 380, facebookSal = 350;
-//
-// var googleHours = parseInt(prompt("How many hours did you work for Google?"), 10);
-// console.log(typeof(googleHours));
-// var amazonHours = parseInt(prompt("How many hours did you work for Amazon?"), 10);
-// console.log(typeof(amazonHours));
-// var facebookHours = parseInt(prompt("How many hours did you work for Facebook?"), 10);
-// console.log(typeof(facebookHours));
-//
-// alert("Your total salary for the week is: $" +
-// (pay(googleSal, googleHours) + pay(amazonSal, amazonHours) + pay(facebookSal, facebookHours)) + ".00");
+function pay(salary, hoursWorked) {
+    return salary * hoursWorked;
+}
+
+var googleSal = parseInt(prompt("How much do you make an hour at Google?"), 10);
+console.log(typeof(googleSal));
+var amazonSal = parseInt(prompt("How much do you make an hour at Amazon?"), 10);
+console.log(typeof(amazonSal));
+var facebookSal = parseInt(prompt("How much do you make an hour at Facebook?"), 10);
+console.log(typeof(facebookSal));
+
+var googleHours = parseInt(prompt("How many hours did you work for Google?"), 10);
+console.log(typeof(googleHours));
+var amazonHours = parseInt(prompt("How many hours did you work for Amazon?"), 10);
+console.log(typeof(amazonHours));
+var facebookHours = parseInt(prompt("How many hours did you work for Facebook?"), 10);
+console.log(typeof(facebookHours));
+
+alert("Your total salary for the week is: $" +
+(pay(googleSal, googleHours) + pay(amazonSal, amazonHours) + pay(facebookSal, facebookHours)) + ".00");
 
 //Data Types Exercise 3-C
-// function canEnroll(full, conflict) {
-//     return (!full && !conflict) ? "You can enroll." : "You cannot enroll.";
-// }
-//
-// var isFull = (parseInt(prompt("How many students are in the class?"), 10) >= 100);
-// console.log(typeof(isFull));
-//
-// alert(canEnroll(isFull, false));
+function canEnroll(full, conflict) {
+    return (!full && !conflict) ? "You can enroll." : "You cannot enroll.";
+}
+
+var isFull = (parseInt(prompt("How many students are in the class?"), 10) >= 100);
+console.log(typeof(isFull));
+
+alert(canEnroll(isFull, false));
 
 //Data Types Exercise 3-D
-// function offerValid(premium, numberOfProducts, expired) {
-//     return (!expired && (numberOfProducts > 2 || premium)) ? "Offer Valid" : "Offer Invalid";
-// }
-//
-// var inputNumberOfProducts = parseInt(prompt("How many products are you ordering?"), 10);
-// console.log(typeof(inputNumberOfProducts));
-//
-// alert(offerValid(false, inputNumberOfProducts, false));
+function offerValid(premium, numberOfProducts, expired) {
+    return (!expired && (numberOfProducts > 2 || premium)) ? "Offer Valid" : "Offer Invalid";
+}
+
+var inputNumberOfProducts = parseInt(prompt("How many products are you ordering?"), 10);
+console.log(typeof(inputNumberOfProducts));
+
+alert(offerValid(false, inputNumberOfProducts, false));
