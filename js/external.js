578f52e04db54bbc277871ed3b0a2884d0c0a3fc
William Stephens <william.a.stephens17@gmail.com>
2019-12-02 14:20:25 -0600

    Refactoring external.js

--- js/external.js
+++ js/external.js
@@ -1,6 +1,6 @@
 "use strict"
 
-//JavaScript with HTML exercises
+//***** JavaScript with HTML exercises *****
 // console.log("Hello from external JavaScript.");
 //
 // alert("Welcome to my website!");
@@ -9,64 +9,74 @@
 //
 // alert("Great, " + favoriteColor + " is my favorite color too!");
 
-//Data Types Exercise 3-A
-function totalPrice(mer, bear, her, pricePerDay) {
-    return ("Renting " + mer.name + " for " + mer.days + " day(s), " +
-            bear.name + " for " + her.days + " day(s), " +
-            " and " + her.name + " for " + her.days + " day(s) " +
-            " at a daily rate of $" + pricePerDay + ".00 costs $" +
-            ((mer.days + bear.days + her.days) * pricePerDay) + ".00.");
+//***** Data Types Exercise 3-A *****
+// function totalPrice(movies, pricePerDay) {
+//     return "Renting " + movies[0].name + " for " + movies[0].days + " day(s), " +
+//             movies[1].name + " for " + movies[1].days + " day(s), " +
+//             " and " + movies[2].name + " for " + movies[2].days + " day(s) " +
+//             " at a daily rate of $" + pricePerDay + " costs $" +
+//             ((movies[0].days + movies[1].days + movies[2].days) * pricePerDay);
+// }
+//
+// var mermaid = {days: 0, name: "The Little Mermaid"};
+// var bear = {days: 0, name: "Brother Bear"};
+// var hercules = {days: 0, name: "Hercules"};
+// var inputMovies = [mermaid, bear, hercules];
+//
+// var inputPricePerDay = Number(prompt("How many dollars does it cost to rent a movie for a day?"));
+// mermaid.days = Number(prompt("How many days did you rent The Little Mermaid for?"));
+// bear.days = Number(prompt("How many days did you rent Brother for?"));
+// hercules.days = Number(prompt("How many days did you rent Hercules for?"));
+//
+// alert(totalPrice(inputMovies, inputPricePerDay));
+
+//***** Data Types Exercise 3-B *****
+// function pay(salary, hoursWorked) {
+//     return salary * hoursWorked;
+// }
+//
+// var googleRate = parseInt(prompt("How much do you make an hour at Google?"), 10);
+// console.log(typeof(googleRate));
+// var amazonRate = parseInt(prompt("How much do you make an hour at Amazon?"), 10);
+// console.log(typeof(amazonRate));
+// var facebookRate = parseInt(prompt("How much do you make an hour at Facebook?"), 10);
+// console.log(typeof(facebookRate));
+//
+// var googleHours = parseInt(prompt("How many hours did you work for Google?"), 10);
+// console.log(typeof(googleHours));
+// var amazonHours = parseInt(prompt("How many hours did you work for Amazon?"), 10);
+// console.log(typeof(amazonHours));
+// var facebookHours = parseInt(prompt("How many hours did you work for Facebook?"), 10);
+// console.log(typeof(facebookHours));
+//
+// alert("Your total salary for the week is: $" +
+// (pay(googleRate, googleHours) + pay(amazonRate, amazonHours) + pay(facebookRate, facebookHours)));
+
+//***** Data Types Exercise 3-C *****
+// function canEnroll(full, conflict) {
+//     return (!full && !conflict) ? "You can enroll." : "You cannot enroll.";
+// }
+//
+// var isFull = (parseInt(prompt("How many students are in the class?"), 10) >= 100);
+// console.log(typeof(isFull));
+//
+// var scheduleConflict = confirm("Do you have any schedule conflicts?");
+// console.log(typeof(scheduleConflict));
+//
+// alert(canEnroll(isFull, scheduleConflict));
+
+//***** Data Types Exercise 3-D *****
+function offerValid(valid, numberOfProducts, premium) {
+    return (valid && (numberOfProducts > 2 || premium)) ? "Offer Valid" : "Offer Invalid";
 }
 
-var mermaid = {days: 3, name: "The Little Mermaid"};
-var bear = {days: 5, name: "Brother Bear"};
-var hercules = {days: 1, name: "Hercules"};
-
-var inputPricePerDay = parseInt(prompt("How many dollars does it cost to rent a movie for a day?"), 10);
-mermaid.days = parseInt(prompt("How many days did you rent The Little Mermaid for?"), 10);
-bear.days = parseInt(prompt("How many days did you rent Brother for?"), 10);
-hercules.days = parseInt(prompt("How many days did you rent Hercules for?"), 10);
-
-alert(totalPrice(mermaid, bear, hercules, inputPricePerDay));
-
-//Data Types Exercise 3-B
-function pay(salary, hoursWorked) {
-    return salary * hoursWorked;
-}
-
-var googleSal = parseInt(prompt("How much do you make an hour at Google?"), 10);
-console.log(typeof(googleSal));
-var amazonSal = parseInt(prompt("How much do you make an hour at Amazon?"), 10);
-console.log(typeof(amazonSal));
-var facebookSal = parseInt(prompt("How much do you make an hour at Facebook?"), 10);
-console.log(typeof(facebookSal));
-
-var googleHours = parseInt(prompt("How many hours did you work for Google?"), 10);
-console.log(typeof(googleHours));
-var amazonHours = parseInt(prompt("How many hours did you work for Amazon?"), 10);
-console.log(typeof(amazonHours));
-var facebookHours = parseInt(prompt("How many hours did you work for Facebook?"), 10);
-console.log(typeof(facebookHours));
-
-alert("Your total salary for the week is: $" +
-(pay(googleSal, googleHours) + pay(amazonSal, amazonHours) + pay(facebookSal, facebookHours)) + ".00");
-
-//Data Types Exercise 3-C
-function canEnroll(full, conflict) {
-    return (!full && !conflict) ? "You can enroll." : "You cannot enroll.";
-}
-
-var isFull = (parseInt(prompt("How many students are in the class?"), 10) >= 100);
-console.log(typeof(isFull));
-
-alert(canEnroll(isFull, false));
-
-//Data Types Exercise 3-D
-function offerValid(premium, numberOfProducts, expired) {
-    return (!expired && (numberOfProducts > 2 || premium)) ? "Offer Valid" : "Offer Invalid";
-}
+var inputValid = confirm("Is the offer still valid?");
+console.log(typeof(inputValid));
 
 var inputNumberOfProducts = parseInt(prompt("How many products are you ordering?"), 10);
 console.log(typeof(inputNumberOfProducts));
 
-alert(offerValid(false, inputNumberOfProducts, false));
+var inputPremium = confirm("Do you have a premium membership?");
+console.log(typeof(inputPremium));
+
+alert(offerValid(inputValid, inputNumberOfProducts, inputPremium));
