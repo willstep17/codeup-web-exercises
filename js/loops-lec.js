46611f27fcd97d069bcc2a46079c7f2453b0a625
William Stephens <william.a.stephens17@gmail.com>
2019-12-06 15:33:36 -0600

    finished with for loops

--- js/loops-lec.js
+++ js/loops-lec.js
@@ -18,11 +18,6 @@
 //     a++;
 // }
 
-
-
-
-
-
 //TODO TOGETHER: Create a while loop that counts down from 20 to 0 (inclusive) by 1
 // (20, 19, 18...0)
 
@@ -31,8 +26,6 @@
 //     console.log(b);
 //     b--;
 // }
-
-
 
 //TODO TOGETHER: Create a while loop that counts up to 20 (inclusive) by 2s (0, 2, 4...)
 // (0, 2, 4...20)
@@ -45,19 +38,28 @@
 
 //TODO TOGETHER: Create a while loop that asks the user to confirm: "Would you like to exit?"
 
-var exit = false;
-while (exit === false) {
-    exit = confirm("Would you like to exit?");
-}
+// var exit = false;
+// while (!exit) {
+//     exit = confirm("Would you like to exit?");
+// }
 
 //TODO: Create a while loop that counts to 100 (inclusive) in increments of ten:
 // (10...20...30...)
 
-
+// var d = 10;
+// while (d <= 100) {
+//     console.log(d);
+//     d+=10;
+// }
 
 //TODO: Create a while loop that counts down from 50 to 1 (inclusive) in increments of 1:
 // (50, 49, 48, 47, 46 .... 1)
 
+// var e = 50;
+// while (e >= 1) {
+//     console.log(e);
+//     e--;
+// }
 
 /*********************************************
  *               DO-WHILE LOOPS
@@ -68,33 +70,46 @@ while (exit === false) {
 //  Code to be run.
 // } while (condition)
 
-
 // var i = 10;
-//
 // while (i < 10) {
-// 	console.log('while loop iteration #' + i);
-// 	i++;
+//     console.log('while loop iteration #' + i);
+//     i++;
 // }
-
 
 // TODO TOGETHER: Why doesnt the example above run?
 //  >>Answer: Because the condition i < 10 is never true
 
-
 // TODO TOGETHER: Refactor the example above using a do-while loop. What behavior do we notice?
 //  >>Answer: We can see how the do block runs one time, then the condition gets checked. Since the condition is false the loop stops running.
 
+// var i = 10;
+// do {
+// 	console.log('while loop iteration #' + i);
+// 	i++;
+// } while (i < 10);
 
 //TODO TOGETHER: Create a do-while loop that asks the user to confirm: "Would you like to exit?" What difference do you notice?
 // >>Answer: we can declare the variable inside the loop, because the do block runs at least once compared to while loop
 
+//Allows us to declare the user input variable in the local scope.
+// do {
+//     var exit = confirm("Would you like to exit?");
+// } while (!exit);
 
 //TODO TOGETHER: Create a do-while loop that counts up to 20 (inclusive) by 2s (0, 2, 4...)
 // (0, 2, 4...20)
 
+//     var j = 0;
+// do {
+//     console.log(j);
+//     j += 2;
+// } while (j <= 20);
 
 //TODO: Create a do-while loop that prompts the user for a color and continues to prompt until the string passed is "blue"
 
+// do {
+//     var color = prompt("Please enter a color. Use 'blue' to exit.");
+// } while (color !== 'blue');
 
 /*********************************************
  *               FOR LOOPS
@@ -110,19 +125,36 @@ while (exit === false) {
 
 // TODO TOGETHER: Write a for loop that loops through numbers 0 - 5 (inclusive);
 
+// for(var i=0;i<=5;i++) {
+//     console.log(i);
+// }
 
 // TODO TOGETHER: Why are loops beneficial?
 
 
 // TODO TOGETHER: Console.log each color from the array randomColors
-//  var randomColors = ["red","orange","yellow","blue","green"]
+
+// var randomColors = ["red","orange","yellow","blue","green"]
+// for(var i=0;i<randomColors.length;i++) {
+//     console.log(randomColors[i]);
+// }
 
 
 // TODO TOGETHER: Using a for loop, log all even numbers 0 - 100;
 
+// for(var i=0;i<=100;i++) {
+//     if(i % 2 === 0) {
+//         console.log(i);
+//     }
+// }
 
 // TODO: Using a for loop, log all numbers divisible by 5, from 0 - 100;
 
+// for(var i=0;i<=100;i++) {
+//     if(i % 5 === 0) {
+//         console.log(i);
+//     }
+// }
 
 // TODO: Using a for loop, complete the FizzBuzz challenge:
 //  Your program should count up to 100.
@@ -131,7 +163,17 @@ while (exit === false) {
 //  If divisible by 3 and 5, log "FizzBuzz"
 //  All other cases, just log the number.
 
-
+for(var i=0;i<100;i++) {
+    if (i % 15 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 5 === 0) {
+        console.log("Fizz");
+    } else if (i % 3 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
 
 /*********************************************
  *              BREAKS & CONTINUES
