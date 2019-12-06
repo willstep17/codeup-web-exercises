03807cb319f8630b5f85627e26544c7416b1cc7f
William Stephens <william.a.stephens17@gmail.com>
2019-12-06 09:24:41 -0600

    Modifying functionWithinFunction.js

--- js/functionWithinFunction.js
+++ js/functionWithinFunction.js
@@ -3,5 +3,11 @@ function outerFunction(callBackFunction) {
 }
 
 outerFunction(function () {
-    alert("It works!");
-});
+    alert("It works from an anonymous function!");
+}); //Passing an anonymous function as a parameter
+
+function passTo() {
+    alert("It works from a declared function!");
+}
+
+outerFunction(passTo); //Passing a declared function as a parameter
