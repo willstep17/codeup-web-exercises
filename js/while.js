c3f9b0d7375eeb5a91357bf82dcd5a90afe6aaca
William Stephens <william.a.stephens17@gmail.com>
2019-12-09 11:51:15 -0600

    updates to cones problem - do while instead of while

--- js/while.js
+++ js/while.js
@@ -1,5 +1,5 @@
 (function(){
-"use strict"
+"use strict";
 
 var x = 2;
 while(x <= 65536) {
@@ -10,21 +10,17 @@ while(x <= 65536) {
 // This is how you get a random number between 50 and 100
 var allCones = Math.floor(Math.random() * 50) + 50;
 
-while(allCones > 0) {
+do {
     console.log(allCones + " cones left to sell");
     var conesThisCustomer = Math.floor(Math.random() * 5) + 1;
     if (conesThisCustomer > allCones) {
         console.log("Cannot sell you " + conesThisCustomer + " cones I only have " + allCones + "...");
-        allCones = 0;
-        console.log("Yay! I sold them all!");
-    } else if (conesThisCustomer === allCones) {
-        allCones -= conesThisCustomer;
-        console.log(conesThisCustomer + " cones sold...");
-        console.log("Yay! I sold them all!");
     } else {
         allCones -= conesThisCustomer;
         console.log(conesThisCustomer + " cones sold...");
     }
-}
+} while(allCones > 0)
+
+console.log("Yay! I sold them all!");
 
 })();
