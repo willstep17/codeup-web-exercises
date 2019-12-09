4a531934d63f6f15f706f3127ea61e1e10732c9b
William Stephens <william.a.stephens17@gmail.com>
2019-12-09 11:01:54 -0600

    Finished break_and_continue.js

--- js/break_and_continue.js
+++ js/break_and_continue.js
@@ -1,14 +1,22 @@
 (function(){
 "use strict";
 
+var skipNumber;
+
 while (true) {
-    var oddNumber = prompt("Please enter an odd number between 1 and 50: ");
-    if(oddNumber % 2 !== 0 && oddNumber >= 1 && oddNumber <= 50) {
+    skipNumber = Number(prompt("Please enter an odd number between 1 and 50: "));
+    if(skipNumber % 2 !== 0 && skipNumber >= 1 && skipNumber <= 50) {
+        console.log("Number to skip is: " + skipNumber);
         break;
     }
 }
 
-console.log(oddNumber);
-
+for(var i=1;i<=50;i+=2) {
+    if(i === skipNumber) {
+        console.log("Yikes! Skipping number: " + i);
+        continue;
+    }
+    console.log("Here is an odd number: " + i);
+}
 
 })();
