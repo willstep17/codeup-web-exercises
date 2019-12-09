082952d69839f52fd2cd4fb02d274dbd364f7bcf
William Stephens <william.a.stephens17@gmail.com>
2019-12-09 12:26:43 -0600

    before lunch commit

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,32 +1,32 @@
 (function(){
 "use strict";
 
-var showMultiplicationTable = function(num) {
-    for(var i=1;i<=10;i++) {
-        console.log(num + " x " + i + " = " + num * i);
-    }
-};
-
-showMultiplicationTable(3);
-showMultiplicationTable(7);
-
-var theRandom;
-for(var j=0;j<10;j++) {
-    theRandom = Math.floor(Math.random() * 180) + 20;
-    theRandom % 2 === 0 ? console.log(theRandom + " is even") : console.log(theRandom + " is odd");
-}
-
-for(var k=1;k<=9;k++) {
-    var logString = "";
-    for(var l=1;l<=k;l++) {
-        logString += k;
-    }
-    console.log(logString);
-}
-
-for(var m=1;m<=9;m++) {
-    console.log(m.toString().repeat(m));
-}
+// var showMultiplicationTable = function(num) {
+//     for(var i=1;i<=10;i++) {
+//         console.log(num + " x " + i + " = " + num * i);
+//     }
+// };
+//
+// showMultiplicationTable(3);
+// showMultiplicationTable(7);
+//
+// var theRandom;
+// for(var j=0;j<10;j++) {
+//     theRandom = Math.floor(Math.random() * 180) + 20;
+//     theRandom % 2 === 0 ? console.log(theRandom + " is even") : console.log(theRandom + " is odd");
+// }
+//
+// for(var k=1;k<=9;k++) {
+//     var logString = "";
+//     for(var l=1;l<=k;l++) {
+//         logString += k;
+//     }
+//     console.log(logString);
+// }
+//
+// for(var m=1;m<=9;m++) {
+//     console.log(m.toString().repeat(m));
+// }
 
 for(var n=100;n>=5;n-=5) {
     console.log(m);
