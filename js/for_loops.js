ee11f32d6333de444960b62cfd81b5e5d86a8f9a
William Stephens <william.a.stephens17@gmail.com>
2019-12-09 12:19:04 -0600

    taking things out of functions in for_loops.js

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,43 +1,35 @@
 (function(){
 "use strict";
 
-// var showMultiplicationTable = function(num) {
-//     for(var i=1;i<=10;i++) {
-//         console.log(num + " X " + i + " = " + num * i);
-//     }
-// }
-//
-// showMultiplicationTable(3);
-// showMultiplicationTable(7);
+var showMultiplicationTable = function(num) {
+    for(var i=1;i<=10;i++) {
+        console.log(num + " x " + i + " = " + num * i);
+    }
+};
 
-// function tenRandom() {
-//     var theRandom;
-//     for(var j=0;j<10;j++) {
-//         theRandom = Math.floor(Math.random() * 180) + 20;
-//         theRandom % 2 === 0 ? console.log(theRandom + " is even") : console.log(theRandom + " is odd");
-//     }
-// }
-//
-// tenRandom();
+showMultiplicationTable(3);
+showMultiplicationTable(7);
 
-// function printNumberOfNumber() {
-//     for(var k=1;k<=9;k++) {
-//         var logString = "";
-//         for(var l=1;l<=k;l++) {
-//             logString += k;
-//         }
-//         console.log(logString);
-//     }
-// }
-//
-// printNumberOfNumber();
+var theRandom;
+for(var j=0;j<10;j++) {
+    theRandom = Math.floor(Math.random() * 180) + 20;
+    theRandom % 2 === 0 ? console.log(theRandom + " is even") : console.log(theRandom + " is odd");
+}
 
-// function subtractFive() {
-//     for(var m=100;m>=5;m-=5) {
-//         console.log(m);
-//     }
-// }
-//
-// subtractFive();
+for(var k=1;k<=9;k++) {
+    var logString = "";
+    for(var l=1;l<=k;l++) {
+        logString += k;
+    }
+    console.log(logString);
+}
+
+for(var m=1;m<=9;m++) {
+    console.log(m.toString().repeat(m));
+}
+
+for(var n=100;n>=5;n-=5) {
+    console.log(m);
+}
 
 })();
