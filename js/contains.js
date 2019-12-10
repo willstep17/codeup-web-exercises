85a9b405abcffd3016c286ec6422c4655a8ecf14
William Stephens <william.a.stephens17@gmail.com>
2019-12-10 09:27:04 -0600

    Update contains.js

--- js/contains.js
+++ js/contains.js
@@ -1,19 +1,26 @@
+(function(){
+"use strict";
+
 var contains = function(arr, thing) {
     return arr.includes(thing);
-}
+};
 
 function containsAlternate(arr, thing) {
     for(var i=0;i<arr.length;i++) {
-        if(thing === animals[i]) {
+        if(animals[i] === thing) {
             return true;
         }
     }
     return false;
 }
 
-var animals = ["Cat", "Dog", "Moose"];
+var animals = ["Cat", "Dog", "Moose", "Catfish", "Starfish", "Turtle"];
 
-console.log(contains(animals, "Cat"));
-console.log(contains(animals, "Bat"));
+console.log(contains(animals, "Cat")); //True
+console.log(contains(animals, "Bat")); //False
 
-console.log(containsAlternate(animals, "Dog"));
+console.log(containsAlternate(animals, "Dog")); //True
+console.log(containsAlternate(animals, "Rabbit")); //False
+console.log(containsAlternate(animals, "Starfish")); //True
+
+})();
