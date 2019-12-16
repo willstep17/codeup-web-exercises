55aebb6b2534a90b9cf6a214aa2f75d2bb4037ed
William Stephens <william.a.stephens17@gmail.com>
2019-12-16 09:26:07 -0600

    santaObject

---
+++ js/santaObject.js
@@ -0,0 +1,18 @@
+(function(){
+"use strict";
+
+var santa = {
+    name: "Santa Claus",
+    niceList: [
+        "Fer",
+        "Vivian",
+        "David"
+    ],
+    sayHello: function(){
+        return "Merry Christmas! Ho Ho Ho!";
+    }
+};
+
+console.log(santa.sayHello());
+
+})();
