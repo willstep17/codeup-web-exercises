f665339f2053dbe40c9daebb51528f3063de37a2
William Stephens <william.a.stephens17@gmail.com>
2020-01-21 15:32:22 -0600

    npm references

--- js/promises.js
+++ js/promises.js
@@ -10,20 +10,40 @@
         //     });
         // };
         //
+        // const waitTwo = (num) => {
+        //     return new Promise((resolve) => {
+        //         setTimeout(() => {
+        //             resolve();
+        //         }, num);
+        //     });
+        // };
+        //
         // wait(3000).then(() => console.log("You\'ll see this after 3 seconds"));
         // wait(6000).then(() => console.log("You\'ll see this after 6 seconds"));
+        // waitTwo(5000).then(() => console.log("You\'ll see this after 5 seconds"));
 
-        const getGithubUserLastDate = function(inputUsername) {
-           return fetch(`https://api.github.com/users/${inputUsername}/events`, {headers: {'Authorization': gitHubPromiseExerciseToken}})
-               .then(response => response.json())
-               .then(events => {
-                   let newCommit = events.find(event => event.type === "PushEvent");
-                   console.log(`The newest commit was made at ${newCommit.created_at}`);
-               })
-               .catch(error => console.error(error));
+        // const getGithubUserLastDate = function(inputUsername) {
+        //    return fetch(`https://api.github.com/users/${inputUsername}/events`, {headers: {'Authorization': gitHubPromiseExerciseToken}})
+        //        .then(response => response.json())
+        //        .then(events => {
+        //            let newCommit = events.find(event => event.type === "PushEvent");
+        //            console.log(`The newest commit was made at ${newCommit.created_at}`);
+        //        })
+        //        .catch(error => console.error(error));
+        // };
+        //
+        // getGithubUserLastDate('willstep17');
+
+        const lastCommit = (username) => {
+            //Return a promise that resolves to time of usernames last commit
+            fetch(`https://api.github.com/users/${username}/events`, {headers: {'Authorization': gitHubPromiseExerciseToken}})
+                .then(data => data.json())
+                .then(data => data.find(event => event.type === "PushEvent"))
+                .then(event => event.created_at)
+                .then(date => console.log(date));
         };
 
-        getGithubUserLastDate('willstep17');
+        lastCommit('willstep17');
 
     });
 })();
