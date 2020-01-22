848faf3162f540f44813bc1d45b3aa6ddf5c27a4
William Stephens <william.a.stephens17@gmail.com>
2020-01-22 11:47:32 -0600

    node-require.js

---
+++ js/node-practice-two.js
@@ -0,0 +1,10 @@
+const car = {
+    year: 2012,
+    model: 'Scion',
+    miles: 80000
+};
+
+const sayHello = () => console.log('Hello from node-practice-two.js');
+
+module.exports = {car, sayHello};
+
